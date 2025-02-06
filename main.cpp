bb21214ea5c2d99a2b671e4bce63d50237595558
Ogan Özkul <Ogan.Oezkul@ceff.ch>
2025-02-06 14:50:43 +0100

    faire en sorte que l'application puisse prendre le fichier

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+
+const std::string helpMessageStr = "you should specify a filename like so\n\nhtmlentities yourfilename.txt";
+const std::string fileOpeningError = "something went wrong while opening the file.\nEither the file doesn't exists or corrupted.";
 
 int main(int argc, const char** argv) {
-	std::cout << "Hello World!" << std::endl;
+
+	if (argc != 2) {
+		std::cout << helpMessageStr << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	std::ifstream inputFile;
+	inputFile.open(argv[1]);
+
+	if (!inputFile.is_open()) {
+		std::cout << fileOpeningError << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	std::ofstream outputFile;
+
 	return EXIT_SUCCESS;
 }
